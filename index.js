90c3f31444bbd1d4f31c4b7df328dbd5489d7568
Duong Thanh Duy <dexter@nexondv.com>
2023-08-04 16:25:55 +0700

    update final TOC html

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import puppeteer from "puppeteer";
-import fs from "fs";
-import json from "./resource.json" assert { type: "json" };
+import puppeteer from 'puppeteer';
+import fs from 'fs';
+import json from './resource.json' assert { type: 'json' };
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
@@ -22,10 +22,10 @@ import json from "./resource.json" assert { type: "json" };
   fs.writeFile(
     `LEGENDARY_MOONLIGHT_SCULPTOR.html`,
     htmlContent,
-    "utf8",
+    'utf8',
     (err) => {
       if (err) console.error(err);
-      console.log("file created");
+      console.log('file created');
     }
   );
 
@@ -40,44 +40,44 @@ const writeChapter = async (page, chapter) => {
   // Set screen size
   await page.setViewport({ width: 2560, height: 1440 });
 
-  await page.waitForSelector(".chapter-title");
-  let chapTitleElement = await page.$(".chapter-title");
+  await page.waitForSelector('.chapter-title');
+  let chapTitleElement = await page.$('.chapter-title');
   let chapTitle = await page.evaluate((el) => el.textContent, chapTitleElement);
-  let currentBook = chapTitle.split(" ").splice(0, 2).join(" ");
+  let currentBook = chapTitle.split(' ').splice(0, 2).join(' ');
 
-  await page.waitForSelector(".chapter-c");
-  let chapContentElement = await page.$(".chapter-c");
+  await page.waitForSelector('.chapter-c');
+  let chapContentElement = await page.$('.chapter-c');
   let chapContent = await page.evaluate(
     (el) => el.innerHTML,
     chapContentElement
   );
 
-  const htmlContent = `<h1>${currentBook}</h1>
-                <h2>${chapTitle}</h2>
+  const htmlContent = `<h2>${currentBook}</h2>
+                <h3>${chapTitle}</h3>
                 ${chapContent}`;
   return htmlContent;
 };
 
 const getResourceChapters = async (page) => {
-  await page.click(".btn.btn-success.btn-chapter-nav.chapter_jump", 10);
+  await page.click('.btn.btn-success.btn-chapter-nav.chapter_jump', 10);
   await page.waitForSelector(
-    ".btn.btn-success.btn-chapter-nav.form-control.chapter_jump"
+    '.btn.btn-success.btn-chapter-nav.form-control.chapter_jump'
   );
   let selectElement = await page.$(
-    ".btn.btn-success.btn-chapter-nav.form-control.chapter_jump"
+    '.btn.btn-success.btn-chapter-nav.form-control.chapter_jump'
   );
   const selectInnerHtml = await page.evaluate(() =>
     Array.from(
       document.querySelectorAll(
-        ".btn.btn-success.btn-chapter-nav.form-control.chapter_jump option"
+        '.btn.btn-success.btn-chapter-nav.form-control.chapter_jump option'
       )
     ).map((element) => element.value)
   );
   let dataResource = selectInnerHtml.slice(
     Math.ceil(selectInnerHtml.length / 2)
   );
-  fs.writeFile("resource.json", JSON.stringify(dataResource), "utf8", (err) => {
+  fs.writeFile('resource.json', JSON.stringify(dataResource), 'utf8', (err) => {
     if (err) console.log(err);
-    console.log("resource created");
+    console.log('resource created');
   });
 };
